d52930856283004517de501f08fe1550e23c45d0
jandos0492 <jandos0492@gmail.com>
2023-07-06 04:02:11 -0400

    Added the console.logs to the psql.setup-script.js file

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,7 +1,15 @@
 const { sequelize } = require("./db/models");
 
+console.log("Starting psql-setup-script.js...");
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+  console.log("showAllSchemas result:", data);
+
   if (!data.includes(process.env.SCHEMA)) {
+    console.log("Creating new schema:", process.env.SCHEMA);
     await sequelize.createSchema(process.env.SCHEMA);
+    console.log("New schema created!");
   }
+
+  console.log("psql-setup-script.js completed!");
 });
