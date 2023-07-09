20367f4cb33205c37e77ab4e61431324a9df5638
jandos0492 <jandos0492@gmail.com>
2023-07-09 19:00:07 -0400

    Added schema to the seeder file

--- backend/config/database.js
+++ backend/config/database.js
@@ -19,12 +19,6 @@ module.exports = {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
     define: {
       schema: process.env.SCHEMA
     }
