69277b7568b33a6ee493892cee6e9f03303861d3
jandos0492 <jandos0492@gmail.com>
2024-10-12 00:35:06 -0400

    missed one letter in url for axios

--- backend/app.js
+++ backend/app.js
@@ -88,7 +88,7 @@ const cyclicFunc = async () => {
   while (true) {
     try {
       // Send request to your /api/keep-alive endpoint
-      await axios.get('https://soccer-player.onrender.com/api/keep-alive');
+      await axios.get('https://soccer-players.onrender.com/api/keep-alive');
       console.log('Keep-alive request made to the server');
 
       // Wait for 14 minutes (14 * 60 * 1000 milliseconds)
