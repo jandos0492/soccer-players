3cc37b7df0e01386233df6c379a4d9755563afb2
jandos0492 <jandos0492@gmail.com>
2024-10-12 00:43:54 -0400

    Still not works the asyclic function updated the router

--- backend/routes/api/keepAliveRouter.js
+++ backend/routes/api/keepAliveRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/keep-alive", (req, res) => {
-    res.status(200).send("Server is alive");
+router.get("/alive", (req, res) => {
+    res.status(200).json({ message: "Server is alive" });
 });
 
 module.exports = router;
